2df782397cf7b32a6da7da8ef8cafd32bd0ac6b9
Tenaria <claire.jfei@gmail.com>
2019-10-09 11:26:18 +1100

    adds missing class to back-end

--- back-end/app/Http/Controllers/eventController.php
+++ back-end/app/Http/Controllers/eventController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use Response;
 
 class eventController extends Controller
 {
 	public function test() {
-		
+		$users = DB::table('users')
+			->get();
+
+		//var_dump('hi');
+		//die(var_dump($users));
+
 		return Response::json([
 			'title' => 'Nice',
         	'body' => 'Hello World!'
 		], 200);
 	}
+
+
 }
