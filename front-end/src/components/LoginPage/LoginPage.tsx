bc7f7938ba657d40bcfe6d053d4afaa87d645c21
robustprogram <robustprogram@gmail.com>
2019-10-15 16:43:03 +1100

    Added code to link frontend + backend

--- front-end/src/components/LoginPage/LoginPage.tsx
+++ front-end/src/components/LoginPage/LoginPage.tsx
@@ -8,9 +8,20 @@ class LoginPage extends React.Component<FormComponentProps, {}> {
 
   handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    this.props.form.validateFieldsAndScroll((err, values) => {
+    this.props.form.validateFieldsAndScroll(async (err, values) => {
       if (!err) {
         console.log('Received values of: ', values);
+        const res = await fetch('http://localhost:8000/log_in', {
+          method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(values)
+        })
+
+        console.log(res);
       }
     })
   }
@@ -22,15 +33,19 @@ class LoginPage extends React.Component<FormComponentProps, {}> {
       <div className="form-wrapper">
         <Form onSubmit={this.handleSubmit}>
           <Title level={2}>LOGIN</Title>
-          <Form.Item label="Username">
-            {getFieldDecorator('username', {
-              rules: [{ required: true, message: 'Please input your username!' }],
-            })(
-              <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="Username"
-              />,
-            )}
+          <Form.Item label="Email">
+            {getFieldDecorator('email', {
+              rules: [{
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },{
+                required: true,
+                message: 'Please input your E-mail!',
+              }],
+            })(<Input
+              placeholder="Email"
+              prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            />)}
           </Form.Item>
           <Form.Item label="Password">
             {getFieldDecorator('password', {
