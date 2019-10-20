1f0b53d77048edbcd43b6aab4026dae2223cdee8
Tenaria <claire.jfei@gmail.com>
2019-10-20 20:29:35 +1100

    added location insertion/update on event creation and edit

--- back-end/app/Helpers/helpers.php
+++ back-end/app/Helpers/helpers.php
@@ -19,7 +19,25 @@ if (!function_exists('validate_jwt')) {
 
 if (!function_exists('proper_empty_check')) {
     function proper_empty_check($string="") {
-       return (str_replace(' ', '', $string) != '');
+        return (str_replace(' ', '', $string) != '');
+    }
+}
+
+if (!function_exists('get_event_attributes_pk')) {
+    function get_event_attributes_pk() {
+        $attributes_array = [];
+        $attributes = DB::table('event_attributes')   
+                        ->select('events_attributes_id', 'events_attributes_name')
+                        ->get();
+
+        foreach($attributes as $attribute) {
+            $name = $attribute->events_attributes_name;
+            $primary_key = $attribute->events_attributes_id;
+            
+            $attributes_array[$name] = $primary_key;
+        }
+
+        return $attributes_array;
     }
 }
 
