7e072028b4cec34f0cc1520904ecc362b2a5377c
Tenaria <claire.jfei@gmail.com>
2019-10-26 15:05:48 +1100

    fixes undefined index

--- back-end/app/Http/Controllers/eventAjaxController.php
+++ back-end/app/Http/Controllers/eventAjaxController.php
@@ -237,7 +237,7 @@ class eventAjaxController extends Controller
 				$event_data = DB::table('events')
 								->where ([
 									['events_active', 1],
-									['events_createdby',$token_data['users_id']],
+									['events_createdby',$token_data['user_id']],
 									['events_id', $event_id]
 								])
 								->first();
@@ -250,7 +250,7 @@ class eventAjaxController extends Controller
 						DB::table('events')
 							->where([
 								['events_active', 1],
-								['events_createdby',$token_data['users_id']],
+								['events_createdby',$token_data['user_id']],
 								['events_id', $event_id]
 							])
 							->update([
@@ -370,7 +370,7 @@ class eventAjaxController extends Controller
 				$event_data = DB::table('events')
 								->where ([
 									['events_active', 1],
-									['events_id',$token_data['users_id']],
+									['events_id',$token_data['user_id']],
 									['events_id', $event_id],
 									['events_status',0]
 									
@@ -443,7 +443,7 @@ class eventAjaxController extends Controller
 				$event_data = DB::table('events')
 								->where ([
 									['events_active', 1],
-									['events_id',$token_data['users_id']],
+									['events_id',$token_data['user_id']],
 									['events_id', $event_id]
 									
 								])
