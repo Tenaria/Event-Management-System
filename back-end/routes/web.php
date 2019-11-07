d99a91cbc98c787f5f1bbd8d349516503d77f57b
Tenaria <claire.jfei@gmail.com>
2019-11-07 20:33:33 +1100

    sort out routes

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -11,50 +11,51 @@
 |
 */
 
+// basic authentication routes
 Route::post('/log_in', 'eventAjaxController@log_in');
 Route::post('/sign_up', 'eventAjaxController@sign_up');
+
+// account routes
 Route::post('/get_account_details', 'eventAjaxController@get_account_details');
 Route::post('/create_account', 'eventController@create_account');
 Route::post('/edit_account', 'eventAjaxController@edit_account');
 Route::post('/get_account_details', 'eventAjaxController@get_account_details');
 
+// event modification routes
 Route::post('/create_event', 'eventAjaxController@create_event');
 Route::post('/edit_event', 'eventAjaxController@edit_event');
-Route::post('/get_event_details', 'eventAjaxController@get_event_details');
 Route::post('/cancel_event', 'eventAjaxController@cancel_event');
+Route::post('/uncancel_event', 'eventAjaxController@uncancel_event');
+Route::post('/mark_as_going','eventAjaxController@mark_as_going');
+Route::post('/unmark_as_going', 'eventAjaxController@unmark_as_going');
+
+// event grabbing routes
 Route::post('/get_upcoming_events', 'eventAjaxController@get_upcoming_events');
 Route::post('/get_invited_events_past', 'eventAjaxController@get_invited_events_past');
 Route::post('/get_invited_events_upcoming', 'eventAjaxController@get_invited_events_upcoming');
-Route::post('/get_emails_exclude_user', 'eventAjaxController@get_emails_exclude_user');
-Route::post('/mark_as_going','eventAjaxController@mark_as_going');
+Route::post('/get_past_events', 'eventAjaxController@get_past_events');
 
 Route::post('/get_attendees_of_event', 'eventAjaxController@get_attendees_of_event');
-Route::post('/get_past_events', 'eventAjaxController@get_past_events');
+Route::post('/get_event_details', 'eventAjaxController@get_event_details');
+Route::post('/search_public_event', 'eventAjaxController@search_public_event');
+
+
+// select routes
+Route::post('/get_emails_exclude_user', 'eventAjaxController@get_emails_exclude_user');
+Route::post('/get_tags', 'eventAjaxController@get_tags');
+
+
+// session routes
 Route::post('/load_event_sessions', 'eventAjaxController@load_event_sessions');
 Route::post('/create_event_sessions', 'eventAjaxController@create_event_sessions');
 Route::post('/edit_event_sessions', 'eventAjaxController@edit_event_sessions');
 Route::post('/remove_event_sessions', 'eventAjaxController@remove_event_sessions');
-Route::post('/search_public_event', 'eventAjaxController@search_public_event');
-Route::post('/get_tags', 'eventAjaxController@get_tags');
-Route::post('/uncancel_event', 'eventAjaxController@uncancel_event');
-Route::get('/', 'eventAjaxController@get_summary_dashboard');
 Route::post('/cancel_event_sessions', 'eventAjaxController@cancel_event_sessions');
 Route::post('/uncancel_event_sessions', 'eventAjaxController@uncancel_event_sessions');
-Route::post('/unmark_as_going', 'eventAjaxController@unmark_as_going');
 
+// timetable routes
 Route::post('/get_timetable_details', 'eventAjaxController@get_timetable_details');
 Route::post('/save_timetable_details', 'eventAjaxController@save_timetable_details');
 
-//SPRINT 4 ????
-
-
-// SPRINT 4 (ROUTES)
-// US-6 (Notify Attendees), US-16 (Mark as Going), US-4 (Cancel Session from Event)
-
-// SPRINT 5 (ROUTES)
-// US10 (Setup Custom Reminders), US-14(Tagging Events), US-15 (Search for an Event), US-17 (Send Invitations)
-
-// SPRINT 6 (ROUTES)
-// US-9 (View Clashing Events)/US-7 (Manage Availabilities), US-13 (Summary Dashboard)
-
-// error_log($fnameInput);
+// other routes
+Route::get('/', 'eventAjaxController@get_summary_dashboard');
