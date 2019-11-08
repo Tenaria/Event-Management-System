a40b51747c52c51ff3dc7520208ba54af5d763c3
Tenaria <claire.jfei@gmail.com>
2019-11-08 18:15:20 +1100

    removes yearly as a possible descriptor from timetables at it is too far ahead in the future

--- back-end/app/Http/Controllers/eventAjaxController.php
+++ back-end/app/Http/Controllers/eventAjaxController.php
@@ -2435,7 +2435,7 @@ class eventAjaxController extends Controller
 				}
 
 				//next we want to check if a valid time descriptor is set
-				if(!is_null($recurring_descriptor) || $recurring_descriptor != "daily" || $recurring_descriptor != "fortnightly" || $recurring_descriptor != "monthly" || $recurring_descriptor != "yearly") {
+				if(!is_null($recurring_descriptor) || $recurring_descriptor != "daily" || $recurring_descriptor != "fortnightly" || $recurring_descriptor != "monthly") {
 					return Response::json(['error' => 'Invalid monthly descriptor passed through'], 400);
 				}
 
