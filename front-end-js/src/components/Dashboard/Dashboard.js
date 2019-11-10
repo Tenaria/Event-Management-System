054bf72ec67b5aecaecea4a9737edadfac46a339
ashx1900 <seahawkc@icloud.com>
2019-11-10 21:27:17 +1100

    I wrote code that allows me to get events from the backend and display it on the dashboard

--- front-end-js/src/components/Dashboard/Dashboard.js
+++ front-end-js/src/components/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
-import { Card, Collapse, Empty, Icon,  Spin, Typography } from 'antd';
+import { Button, Card, Collapse, Empty, Icon, Row, Spin, Tag, Typography, Col } from 'antd';
 import React from 'react';
 
 import TokenContext from '../../context/TokenContext';
 
 const { Title } = Typography;
+const { Panel } = Collapse;
 const spinIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
 
 class Dashboard extends React.Component {
@@ -40,7 +41,6 @@ class Dashboard extends React.Component {
       loadData('http://localhost:8000/get_invited_events_upcoming'),
       loadData('http://localhost:8000/search_public_event'),
     ]).then(values => {
-      console.log(values);
       this.setState({
         upcomingEventsMe: values[0].events,
         upcomingEventsInvited: values[1].events,
@@ -51,22 +51,130 @@ class Dashboard extends React.Component {
   }
 
   render() {
-    const { upcomingEventsMe, upcomingEventsInvited, upcomingEventsPublic } = this.state;
-
-    const sliderStyle = {
-      backgroundColor: '#2D3748',
-      padding: '0em 2.2em',
-      marginBottom: '2em'
+    const { upcomingEventsMe, upcomingEventsInvited, upcomingEventsPublic, loaded } = this.state;
+  
+    const cardStyle = {
+      margin: '1%',
+      width: '30%'
     };
 
     const spinStyle = {
       padding: '2em',
       textAlign: 'center'
     }
+
+    const myUpcomingEventElms = [];
+    const myUpcomingInvitedElms = [];
+    const upcomingElms = [];
+
+    for (let i in upcomingEventsMe) {
+      let event = upcomingEventsMe[i];
+
+      const tagElms = event.events_tags.map(tag =>
+        <Tag>{tag}</Tag>
+      );
+
+      myUpcomingEventElms.push(
+        <Card
+          className="my-event-card"
+          key={i}
+          style={cardStyle}
+          size="small"
+          title={event.events_name}
+        >
+          <div>{tagElms}</div>
+          <p>{event.events_desc ? event.events_desc : 'No description'}</p>
+          <Row style={{
+            position: 'absolute',
+            right: '1em',
+            bottom: '1em'
+          }}>
+            <Button
+              icon="eye"
+              type="primary"
+              onClick={() => this.selectEvent(event.events_id)}
+            />
+          </Row>
+        </Card>
+      );
+    }
+
+    for (let i in upcomingEventsMe) {
+      let event = upcomingEventsMe[i];
+
+      const tagElms = event.events_tags.map(tag =>
+        <Tag>{tag}</Tag>
+      );
+
+      myUpcomingEventElms.push(
+        <Card
+          className="my-event-card"
+          key={i}
+          style={cardStyle}
+          size="small"
+          title={event.events_name}
+        >
+          <div>{tagElms}</div>
+          <p>{event.events_desc ? event.events_desc : 'No description'}</p>
+          <Row style={{
+            position: 'absolute',
+            right: '1em',
+            bottom: '1em'
+          }}>
+            <Button
+              icon="eye"
+              type="primary"
+              onClick={() => this.selectEvent(event.events_id)}
+            />
+          </Row>
+        </Card>
+      );
+    }
+
+    for (let i in upcomingEventsMe) {
+      let event = upcomingEventsMe[i];
+
+      const tagElms = event.events_tags.map(tag =>
+        <Tag>{tag}</Tag>
+      );
+
+      myUpcomingEventElms.push(
+        <Card
+          className="my-event-card"
+          key={i}
+          style={cardStyle}
+          size="small"
+          title={event.events_name}
+        >
+          <div>{tagElms}</div>
+          <p>{event.events_desc ? event.events_desc : 'No description'}</p>
+          <Row style={{
+            position: 'absolute',
+            right: '1em',
+            bottom: '1em'
+          }}>
+            <Button
+              icon="eye"
+              type="primary"
+              onClick={() => this.selectEvent(event.events_id)}
+            />
+          </Row>
+        </Card>
+      );
+    }
   
     return (
       <div>
         <Title level={2}>Dashboard</Title>
+        <Collapse defaultActiveKey={['1', '2', '3']}>
+          <Panel header="My Upcoming Events" key="1">
+            {myUpcomingEventElms}
+          </Panel>
+          <Panel header="My Upcoming Invited Events" key="2">
+          </Panel>
+          <Panel header="Upcoming Public Events" key="3">
+          </Panel>
+        </Collapse>
       </div>
     );
   }
