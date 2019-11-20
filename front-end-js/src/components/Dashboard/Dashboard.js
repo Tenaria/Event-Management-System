bc73ad810c02627dad4224d75bfa50b24f67f3e6
robustprogram <robustprogram@gmail.com>
2019-11-20 14:09:26 +1100

    Updated dashboard

--- front-end-js/src/components/Dashboard/Dashboard.js
+++ front-end-js/src/components/Dashboard/Dashboard.js
@@ -1,191 +1,205 @@
-import { Button, Card, Collapse, Row, Tag, Typography } from 'antd';
-import { Redirect } from "react-router-dom";
+import { message, Row, Typography, Button } from 'antd';
 import React from 'react';
+import {
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, Pie, Sector
+} from 'recharts';
 
 import TokenContext from '../../context/TokenContext';
 
 const { Title } = Typography;
-const { Panel } = Collapse;
+
+const colourCombo = [
+  '#F56565',
+  '#ED8936',
+  '#ECC94B',
+  '#48BB78',
+  '#38B2AC',
+  '#4299E1',
+  '#667EEA',
+  '#9F7AEA',
+  '#ED64A6'
+];
+                   
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
+    payload, percent, value } = props;
+  const fill = payload.colour;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? 'start' : 'end';
+
+  return (
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#999">
+        {`(${(percent * 100).toFixed(2)}%)`}
+      </text>
+    </g>
+  );
+};
 
 class Dashboard extends React.Component {
   state = {
-    upcomingEventsMe: [],
-    upcomingEventsInvited: [],
-    upcomingEventsPublic: [],
-    viewedEvent: false,
+    activeIndex: 0,
+    tagIndex: 0,
+    data: null,
     loaded: false
   }
 
-  componentDidMount = () => {
+  componentDidMount = async () => {
     const { token } = this.context;
     
-    const loadData = url => new Promise(async (resolve, reject) => {
-      const res = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ token })
-      });
-
-      if (res.status === 200) {
-        const data = await res.json();
-        console.log(url, data);
-        resolve(data);
-      } else {
-        resolve({events: []});
-      }
+    const res = await fetch('http://localhost:8000/get_summary_dashboard', {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ token })
     });
 
-    Promise.all([
-      loadData('http://localhost:8000/get_summary_dashboard')
-    ]).then(values => {
-      console.log(values);
-    })
+    const data = await res.json();
+    if (res.status === 200) {
+      console.log(data);
+      this.setState({data: data, loaded: true});
+    } else {
+      message.error(data.error);
+    }
   }
+
+  changeTagIndex = id => this.setState({tagIndex: id});
 
   selectEvent = id => {
     sessionStorage.setItem('event_id', id);
     this.setState({ viewedEvent: true });
   }
 
-  render() {
-    const {
-      upcomingEventsMe,
-      upcomingEventsInvited,
-      upcomingEventsPublic,
-      loaded,
-      viewedEvent
-    } = this.state;
-  
-    const cardStyle = {
-      margin: '1%',
-      width: '30%'
-    };
+  onPieEnter = (data, index) => this.setState({activeIndex: index});
 
+  render() {
+    const { tagIndex, data, loaded } = this.state;
     const spinStyle = {
       padding: '2em',
       textAlign: 'center'
+    };
+    let parsedAttendees = [];
+    let parsedTags = [];
+
+    if (loaded) {
+      parsedAttendees = [{
+        name: 'Attendence',
+        'Last Week': data.last_week_attended_count,
+        'This Week': data.next_week_attend_count 
+      }];
+
+      let tagData = {};
+      switch(tagIndex) {
+        case 0:
+          tagData = data.tags_last_week;
+          break;
+        case 1:
+          tagData = data.tags_this_week;
+          break;
+        case 2:
+          tagData = data.tags_next_week;
+          break;
+        case 3:
+          tagData = data.tags_distribution;
+          break;
+      }
+
+      let i = 0;
+      for (let k in tagData) {
+        parsedTags.push({
+          name: k,
+          value: parseInt(data.tags_last_week[k]),
+          colour: colourCombo[i % colourCombo.length]
+        });
+        i++;
+      }
     }
 
-    const myUpcomingEventElms = [];
-    const myUpcomingInvitedElms = [];
-    const upcomingElms = [];
-
-    for (let i in upcomingEventsMe) {
-      let event = upcomingEventsMe[i];
-
-      const tagElms = event.events_tags.map(tag =>
-        <Tag>{tag}</Tag>
-      );
-
-      myUpcomingEventElms.push(
-        <Card
-          className="my-event-card"
-          key={i}
-          style={cardStyle}
-          size="small"
-          title={event.events_name}
-        >
-          <div>{tagElms}</div>
-          <p>{event.events_desc ? event.events_desc : 'No description'}</p>
-          <Row style={{
-            position: 'absolute',
-            right: '1em',
-            bottom: '1em'
-          }}>
-            <Button
-              icon="eye"
-              type="primary"
-              onClick={() => this.selectEvent(event.events_id)}
-            />
-          </Row>
-        </Card>
-      );
-    }
-
-    for (let i in upcomingEventsInvited) {
-      let event = upcomingEventsInvited[i];
-
-      const tagElms = event.events_tags.map(tag =>
-        <Tag>{tag}</Tag>
-      );
-
-      myUpcomingInvitedElms.push(
-        <Card
-          className="my-event-card"
-          key={i}
-          style={cardStyle}
-          size="small"
-          title={event.events_name}
-        >
-          <div>{tagElms}</div>
-          <p>{event.events_desc ? event.events_desc : 'No description'}</p>
-          <Row style={{
-            position: 'absolute',
-            right: '1em',
-            bottom: '1em'
-          }}>
-            <Button
-              icon="eye"
-              type="primary"
-              onClick={() => this.selectEvent(event.events_id)}
-            />
-          </Row>
-        </Card>
-      );
-    }
-
-    for (let i in upcomingEventsPublic) {
-      let event = upcomingEventsPublic[i];
-
-      const tagElms = event.events_tags.map(tag =>
-        <Tag>{tag}</Tag>
-      );
-
-      upcomingElms.push(
-        <Card
-          className="my-event-card"
-          key={i}
-          style={cardStyle}
-          size="small"
-          title={event.events_name}
-        >
-          <div>{tagElms}</div>
-          <p>{event.events_desc ? event.events_desc : 'No description'}</p>
-          <Row style={{
-            position: 'absolute',
-            right: '1em',
-            bottom: '1em'
-          }}>
-            <Button
-              icon="eye"
-              type="primary"
-              onClick={() => this.selectEvent(event.events_id)}
-            />
-          </Row>
-        </Card>
-      );
-    }
-
-    console.log(viewedEvent);
-  
     return (
-      <div>
-        {viewedEvent ? <Redirect to="/event_details" /> : ''}
-        <Title level={2}>Dashboard</Title>
-        <Collapse defaultActiveKey={['1', '2', '3']}>
-          <Panel header="My Upcoming Events" key="1">
-            <Row type="flex">{myUpcomingEventElms}</Row>
-          </Panel>
-          <Panel header="My Upcoming Invited Events" key="2">
-            <Row type="flex">{myUpcomingInvitedElms}</Row>
-          </Panel>
-          <Panel header="Upcoming Public Events" key="3">
-            <Row type="flex">{upcomingElms}</Row>
-          </Panel>
-        </Collapse>
-      </div>
+      <Row type="flex" justify="space-around">
+        <div style={{textAlign: 'center', marginBottom: '1em', width: 300}}>
+          <Title level={4}>Attendence</Title>
+          <BarChart
+            width={300}
+            height={300}
+            data={parsedAttendees}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="Last Week" fill="#8884d8" />
+            <Bar dataKey="This Week" fill="#82ca9d" />
+          </BarChart>
+        </div>
+        <div style={{textAlign: 'center', width: 375}}>
+          <Title level={4}>Tag Information</Title>
+          <Row>
+            <Button.Group>
+              <Button
+                type={tagIndex === 0 ? 'primary' : ''}
+                onClick={() => this.changeTagIndex(0)}
+              >Last Week</Button>
+              <Button
+                type={tagIndex === 1 ? 'primary' : ''}
+                onClick={() => this.changeTagIndex(1)}
+              >This Week</Button>
+              <Button
+                type={tagIndex === 2 ? 'primary' : ''}
+                onClick={() => this.changeTagIndex(2)}
+              >Next Week</Button>
+              <Button
+                type={tagIndex === 3 ? 'primary' : ''}
+                onClick={() => this.changeTagIndex(3)}
+              >Overview</Button>
+            </Button.Group>
+          </Row>
+          <PieChart width={375} height={300}>
+            <Pie 
+              activeIndex={this.state.activeIndex}
+              activeShape={renderActiveShape} 
+              data={parsedTags} 
+              cx={200} 
+              cy={125} 
+              innerRadius={60}
+              outerRadius={80} 
+              fill="#8884d8"
+              onMouseEnter={this.onPieEnter}
+            />
+          </PieChart>
+        </div>
+      </Row>
     );
   }
 }
